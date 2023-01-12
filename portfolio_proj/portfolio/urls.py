ce61560ec6a18466c9676847f95c3f92bf60e1df
Robert Hassenmayer <hassenr@protonmail.com>
2023-01-12 14:36:30 -0500

    portfolio filly functional with django

--- portfolio_proj/portfolio/urls.py
+++ portfolio_proj/portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-name = "portfolio"
+app_name = "portfolio"
 urlpatterns = [
+    # path("resume/", views.resume_detail_view, name="resume"),
+    path("projects/", views.project_list_view, name="projects"),
     path("", views.index_view, name="index"),
 ]
